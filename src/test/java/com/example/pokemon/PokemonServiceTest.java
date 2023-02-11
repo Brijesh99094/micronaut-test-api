8b4decd058c29910ad0f74592d6203ec9f5fb2ba
Jaypal Mudaliyar <jaypal.mudaliyar24@gmail.com>
2023-02-11 17:06:07 +0530

    ENH: Added Unit Test for getting all pokemons

--- src/test/java/com/example/pokemon/PokemonServiceTest.java
+++ src/test/java/com/example/pokemon/PokemonServiceTest.java
@@ -7,36 +7,66 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.mockito.ArgumentMatchers.*;
+
 class PokemonServiceTest {
 
-  PokemonService pokemonService;
-  PokemonCreationForm pokemonCreationForm;
-  PokemonService mockedService;
-  PokemonRepository pokemonRepository;
+    PokemonService pokemonService;
+    PokemonCreationForm pokemonCreationForm;
+    PokemonService mockedService;
+    PokemonRepository pokemonRepository;
+    PowerService powerService;
 
-  PowerService powerService;
+    Pokemon bulbasaur, pikachu;
 
-  @BeforeEach
-  void setUp() {
-    pokemonRepository = Mockito.mock(PokemonRepository.class);
-    powerService = Mockito.mock(PowerService.class);
-    pokemonService = new PokemonService(pokemonRepository, powerService);
-  }
+    @BeforeEach
+    void setUp() {
+        bulbasaur = new Pokemon(1, "bulbasaur", new Power(1, "grass"), "bulbasaur.png");
+        pikachu = new Pokemon(2, "pikachu", new Power(2, "thunder"), "pikachu.png");
+        pokemonRepository = Mockito.mock(PokemonRepository.class);
+        powerService = Mockito.mock(PowerService.class);
+        pokemonService = new PokemonService(pokemonRepository, powerService);
+    }
 
-  @Test
-  public void testAddPokemon() {
-    // given
-    pokemonCreationForm = new PokemonCreationForm("sagar", 2, "sagarurl");
+    @Test
+    public void testAddPokemon() {
+        // given
+        pokemonCreationForm = new PokemonCreationForm("sagar", 2, "sagarurl");
+        Power expectedPower = new Power(2, "grass");
+        Pokemon expectedPokemon = new Pokemon(null,"sagar", expectedPower, "sagarUrl");
+        Mockito.when(powerService.getById(pokemonCreationForm.getPower()))
+                .thenReturn(expectedPower);
+        Mockito.when(pokemonRepository.save(any(Pokemon.class)))
+                .thenReturn(expectedPokemon);
 
-   //when
-    pokemonService.addPokemon(pokemonCreationForm);
-  //then
-    Mockito.verify(pokemonRepository).findByName(pokemonCreationForm.getName());
-    Mockito.verify(powerService).getById(pokemonCreationForm.getPower());
-    Mockito.verify(pokemonRepository).save(any());
-  }
+        // when
+        Pokemon recievedPokemon = pokemonService.addPokemon(pokemonCreationForm);
 
-  @Test
-  public void shouldReturnListOfPokemons() {}
+        //then
+        Mockito.verify(pokemonRepository).findByName(pokemonCreationForm.getName());
+        Mockito.verify(powerService).getById(pokemonCreationForm.getPower());
+        Mockito.verify(pokemonRepository).save(any(Pokemon.class));
+
+        Assertions.assertThat(recievedPokemon).isEqualTo(expectedPokemon);
+    }
+
+    @Test
+    public void shouldReturnListOfPokemon() {
+        // given
+        Mockito.when(pokemonRepository.findAll())
+                .thenReturn(List.of(bulbasaur, pikachu));
+
+        // when
+        List<Pokemon> pokemonList = pokemonService.get();
+
+        //then
+        Mockito.verify(pokemonRepository).findAll();
+        Assertions.assertThat(pokemonList)
+                .containsExactlyInAnyOrderElementsOf
+                        (List.of(pikachu, bulbasaur));
+    }
+
 }
