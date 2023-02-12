1c4610ed3ae4408708e2df413e212d8d32618019
Brijesh99094 <prajapatibrijesh655@gmail.com>
2023-02-12 21:41:31 +0530

    ENH: Added unit test for getting a correct power object by given power id

--- src/test/java/com/example/power/PowerServiceTest.java
+++ src/test/java/com/example/power/PowerServiceTest.java
@@ -1,8 +1,12 @@
 package com.example.power;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.Optional;
 
 class PowerServiceTest {
 
@@ -10,17 +14,25 @@ class PowerServiceTest {
 
   @BeforeMethod
   public void setUp() {
-    powerRepository = Mockito.mock(PowerRepository.class);
-
 
   }
 
   @Test
   public  void shouldReturnPower(){
+      powerRepository = Mockito.mock(PowerRepository.class);
       PowerService powerService = new PowerService(powerRepository);
+      Power expectedPower = new Power(2,"grass");
+      //given
+      Mockito.when(powerRepository.findById(2)).thenReturn(Optional.of(expectedPower));
+
+      //when
+      Power receivedPower =  powerService.getById(2);
+
+      //then
+      Mockito.verify(powerRepository).findById(2);
 
 
-      Power recievedPower =  powerService.getById(2);
+      Assertions.assertThat(receivedPower).isEqualTo(expectedPower);
   }
 
 
